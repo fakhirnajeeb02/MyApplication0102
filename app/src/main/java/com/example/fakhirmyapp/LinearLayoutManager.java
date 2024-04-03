03a7f97487ced5315b0bfa09cfa517261d102d78
fakhirnajeeb02 <fakhir85724@gmail.com>
2024-04-03 19:16:17 +0500

    CRUD android application using android studio and firebase

---
+++ app/src/main/java/com/example/fakhirmyapp/LinearLayoutManager.java
@@ -0,0 +1,9 @@
+package com.example.fakhirmyapp;
+
+import android.view.ViewGroup;
+
+public class LinearLayoutManager extends ViewGroup.LayoutParams {
+    public LinearLayoutManager(MainActivity mainActivity) {
+        super();
+    }
+}
---
+++ app/src/main/java/com/example/fakhirmyapp/RecyclerView.java
@@ -0,0 +1,11 @@
+package com.example.fakhirmyapp;
+
+public class RecyclerView {
+    public void setLayoutManager(LinearLayoutManager linearLayoutManager) {
+
+    }
+
+    public void setAdapter(TaskAdapter taskAdapter) {
+
+    }
+}
